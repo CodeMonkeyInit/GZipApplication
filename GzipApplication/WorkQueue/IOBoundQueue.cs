8823f6dfaf09cc9e34d83c8c0e8269c02f3cbb48
CodeMonkeyInit <kyliev09@gmail.com>
2020-03-07 17:18:36 +0300

    Replaced Thread.Sleep with proper synchronisation

--- GzipApplication/WorkQueue/IOBoundQueue.cs
+++ GzipApplication/WorkQueue/IOBoundQueue.cs
@@ -1,45 +1,46 @@
-using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
+using GzipApplication.ReaderWriter;
 
-namespace GzipApplication.ReaderWriter
+namespace GzipApplication.WorkQueue
 {
-    public class EvaluationQueue
+    public class IOBoundQueue
     {
         private readonly ConcurrentQueue<Function> _actions = new ConcurrentQueue<Function>();
 
-        private readonly HashSet<string> incompletedFunctions = new HashSet<string>();
+        private readonly HashSet<string> _incompletedFunctions = new HashSet<string>();
+        
+        private readonly AutoResetEvent _newActionEnqueued = new AutoResetEvent(false);
 
         public void Enqueue(Function function)
         {
             _actions.Enqueue(function);
+            _newActionEnqueued.Set();
         }
 
         public void Evaluate() => Evaluate(CancellationToken.None);
 
-        void Evaluate(CancellationToken token)
+        public void Evaluate(CancellationToken token)
         {
             do
             {
+                _newActionEnqueued.WaitOne();
+
                 if (token.IsCancellationRequested)
                     return;
                 
-
                 while (_actions.TryDequeue(out Function function))
                 {
                     var isCompleted = function.Payload();
 
                     if (isCompleted)
-                        incompletedFunctions.Remove(function.Name);
+                        _incompletedFunctions.Remove(function.Name);
 
                     else
-                        incompletedFunctions.Add(function.Name);
+                        _incompletedFunctions.Add(function.Name);
                 }
-
-                Thread.Sleep(TimeSpan.FromMilliseconds(100));
-            } while (incompletedFunctions.Count > 0);
+            } while (_incompletedFunctions.Count > 0);
         }
     }
 }
